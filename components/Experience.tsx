4cc08ed2b60802969c5d8ba02156c0381a712801
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2024-02-03 16:13:05 +0330

    chore: Experince component completed . experience cards added

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -13,7 +13,17 @@ const Experience: React.FunctionComponent<
       whileInView={{opacity:1}}
       transition={{duration:1.5}}
       className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
-    >    
+    >
+      <h3 className="absolute top-14 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Experience
+      </h3>
+
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory mt-10">
+        <ExperienceCard  />
+        <ExperienceCard  />
+        <ExperienceCard  />
+        <ExperienceCard  />
+      </div>      
     </motion.div>
   );
 };
