9552ab751355f463fd3b5ee34c18fe70ef1aaaba
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2024-02-17 12:06:45 +0330

    chore: Hero component completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,6 +33,21 @@ export default function Hero({}: Props) {
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
