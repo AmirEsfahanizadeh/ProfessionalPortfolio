27b2219a623d230e68b51a95f2bebf0a77a5d8e2
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2024-02-19 14:01:02 +0330

    chore: Skill component completed

--- components/skill.tsx
+++ components/skill.tsx
@@ -11,6 +11,17 @@ const Skill: React.FunctionComponent<SkillProps> = ({ directionLeft, src, percen
   return (
     <div className="group relative flex cursor-pointer">
       <motion.img
+      initial={{
+        x: directionLeft? -200 : 200,
+        opacity: 0,
+      }}
+      transition={{
+        duration: 1.2,
+      }}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+      }}
       src={src}
       className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition
       duration-300 ease-in-out md:w-28 md:h-28"
